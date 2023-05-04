4e9b4f23e683308f435fcca1611c36afa700ba9c
Zachary Weston <zachary.weston@propellerhead.co.nz>
2023-05-04 15:42:30 +1200

    added strongly typed promise

--- src/start.ts
+++ src/start.ts
@@ -1,27 +1,33 @@
 import * as express from 'express';
+import { Application } from 'express';
 
 import * as bodyParser from 'body-parser';
 import mongoose, { ConnectOptions } from 'mongoose';
 
 import { router } from './routes';
 
-const run = () => mongoose.connect(
+const run = (): Promise<typeof mongoose> => mongoose.connect(
     `${process.env.MONGO_URI}/info`,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    } as ConnectOptions,
+    } as ConnectOptions
 );
-run();
 
-const app: express.Application = express();
+const app: Application = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use('/api/users', router);
 
-app.listen(port, () => {
-    console.log(`User service listening on port ${port}`);
-});
-
-module.exports = app;
+run()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`User service listening on port ${port}`);
+        });
+    })
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
