477537b0d89317fca2481c9e9eca7c17a5110bca
Zachary Weston <zachary.weston@propellerhead.co.nz>
2023-05-04 19:11:45 +1200

    tidied startup code

--- src/start.ts
+++ src/start.ts
@@ -1,32 +1,43 @@
 import * as express from 'express';
 import { Application } from 'express';
 
-import * as bodyParser from 'body-parser';
-import * as mongoose from 'mongoose';
+import { connect, ConnectOptions } from 'mongoose';
 
 import { router } from './routes';
+import { userModel } from './services/UserService';
+
+const serve = (): void => {
+    const app: Application = express();
+    const port = process.env.PORT || 3000;
+
+    app.use(express.json());
+    app.use('/api/users', router);
+
+    app.listen(port, () => {
+        // connected to express
+        console.log(`User service listening on port ${port}`);
+    });
+}
 
 // await for mongo connection before starting the server
 const run = async (): Promise<void> => {
     try {
         // connected to mongo
         console.log("Connected to MongoDB");
-        await mongoose.connect(`${process.env.MONGO_URI}/info`)
-        const app: Application = express();
-        const port = process.env.PORT || 3000;
 
-        app.use(express.json());
-        app.use(bodyParser.json());
-        app.use('/api/users', router);
+        await connect(`${process.env.MONGO_URI}/info`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            autoIndex: true
+        } as ConnectOptions);
 
-        app.listen(port, () => {
-            // connected to express
-            console.log(`User service listening on port ${port}`);
-        });
+        await userModel.createIndexes();
+        serve();
     }
     catch (error) {
         console.error(error);
         process.exit(1);
     }
 }
+
 run();
